18db89038b26b6d2ea652f1dc6ded02ab55b6b17
Eloi DU BOIS <eloi.du.bois@gmail.com>
2015-06-23 19:06:49 +0200

    ofxPlugins: filterCleaner: better orange mask color constants.

--- ofxPlugins/filterCleaner/src/FilterCleanerDefinitions.hpp
+++ ofxPlugins/filterCleaner/src/FilterCleanerDefinitions.hpp
@@ -19,15 +19,15 @@ static const std::string kParamHelpString( "Remove orange filter that appears on
 
 static const std::string kParamRedFilterColor( "Red filter color" );
 static const std::string kParamRedFilterColorLabel( "Red filter color" );
-static const unsigned short kParamDefaultRedFilterColor( 240 );
+static const unsigned short kParamDefaultRedFilterColor( 247 );
 
 static const std::string kParamGreenFilterColor( "Green filter color" );
 static const std::string kParamGreenFilterColorLabel( "Green filter color" );
-static const unsigned short kParamDefaultGreenFilterColor( 140 );
+static const unsigned short kParamDefaultGreenFilterColor( 133 );
 
 static const std::string kParamBlueFilterColor( "Blue filter color" );
 static const std::string kParamBlueFilterColorLabel( "Blue filter color" );
-static const unsigned short kParamDefaultBlueFilterColor( 80 );
+static const unsigned short kParamDefaultBlueFilterColor( 78 );
 
 static const std::string kParamAlgorithm( "Algorithm" );
 static const std::string kParamAlgorithmRGBReduction( "RGB reduction" );
